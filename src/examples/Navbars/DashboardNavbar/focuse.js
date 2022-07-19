dc07b7e444bd1c0bdf7a85eb96cb1e290fd5e008
Mouhcine MESMOUKI <79112356+override7@users.noreply.github.com>
2022-07-19 02:43:32 +0100

    removed console.log tests Co-authored-by: DIB3 simo7.denguir@gmail.com

--- src/examples/Navbars/DashboardNavbar/focuse.js
+++ src/examples/Navbars/DashboardNavbar/focuse.js
@@ -2,10 +2,10 @@ window.addEventListener("keydown", function (e) {
     if (e.keyCode === 119 || (e.ctrlKey && e.keyCode === 75)) {
         if (document.getElementById("Searchid") !== document.activeElement) {
             e.preventDefault();
-            console.log("Search is not in focus");
+            //console.log("Search is not in focus");
             document.getElementById("Searchid").focus();
         } else {
-            console.log("Default action of CtrlF");
+            //console.log("Default action of CtrlF");
             return true;
         }
     }
