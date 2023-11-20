c2cff3c990207f5de9ddc9fb3480fef6308ae781
Mouhcine <mouhcine.mesmouki7@gmail.com>
2023-11-20 15:11:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@
      # An example resource that does nothing.
      resource "null_resource" "example" {
        triggers = {
-         value = "A example resource that does nothing!"
+         value = "Push to terraform!"
        }
      }
